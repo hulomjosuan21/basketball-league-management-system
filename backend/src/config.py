0428d8fc7c5af7153e42ba7101e6872033ae5501
Josuan Leonardo Hulom <hulomjosuan@gmail.com>
2025-06-10 10:30:09 +0800

    d10.1 config fix

--- backend/src/config.py
+++ backend/src/config.py
@@ -5,8 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    # SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres.elsjyrcrdjfniwclmecx:cBwM5C0rGlcTJp5k@aws-0-us-east-2.pooler.supabase.com:5432/postgres"
+    SQLALCHEMY_DATABASE_URI = os.getenv("SUPABASE_DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'super-secret')
