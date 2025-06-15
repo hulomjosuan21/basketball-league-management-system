5699a51f5da70275e37f8787b46d00bfc819a5bc
Josuan Leonardo Hulom <hulomjosuan@gmail.com>
2025-06-15 22:01:13 +0800

    d15.3 week 1 done

--- bogoballers/lib/main.dart
+++ bogoballers/lib/main.dart
@@ -10,7 +10,7 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-/// Josuan leonardo Hulom
+/// 15/06/2025
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
